44fcbf87f0d7ce1f0c83234644f71aea32009250
Alec Missine <alec_missine@yahoo.com>
2019-03-31 14:12:55 -0400

    Monitor payments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const StellarSdk = require('stellar-sdk')
 const fetch = require('node-fetch')
-const pair = process.argv.length < 3 ? StellarSdk.Keypair.random() :
-  StellarSdk.Keypair.fromSecret(process.argv[2])
-const server = new StellarSdk.Server("https://horizon-testnet.stellar.org")
+const pair = process.argv.length < 3 ? StellarSdk.Keypair.random()
+  : StellarSdk.Keypair.fromSecret(process.argv[2])
+const server = new StellarSdk.Server('https://horizon-testnet.stellar.org')
 
 StellarSdk.Network.useTestNetwork()
 
@@ -10,13 +10,13 @@ if (process.argv.length < 3) createNewAccount()
 else workWithExistingAccounts()
 
 function createNewAccount () {
-  const addr = 
+  const addr =
     `https://friendbot.stellar.org?addr=${encodeURIComponent(pair.publicKey())}`
 
   console.log(pair.secret())
   fetch(addr)
-  .then(res => res.json())
-  .then(json => console.log(json))
+    .then(res => res.json())
+    .then(json => console.log(json))
 }
 
 function workWithExistingAccounts () {
@@ -30,31 +30,59 @@ function sendMoney () {
   var transaction
 
   server.loadAccount(dstId)
-  .catch(StellarSdk.NotFoundError, error => console.error(error))
-  .then(() => { return server.loadAccount(pair.publicKey()) })
-  .then(sourceAccount => {
-    transaction = new StellarSdk.TransactionBuilder(sourceAccount, { fee: 111 })
-    .addOperation(StellarSdk.Operation.payment({
-      destination: dstId,
-      asset: StellarSdk.Asset.native(),
-      amount: process.argv[4]
-    }))
-    .addMemo(StellarSdk.Memo.text('Test Transaction'))
-    .setTimeout(180)
-    .build()
-    transaction.sign(pair)
-    return server.submitTransaction(transaction)
-  })
-  .then(result => logAccountInfo(srcId))
-  .catch(error => console.error(error))
+    .catch(StellarSdk.NotFoundError, error => console.error(error))
+    .then(() => { return server.loadAccount(pair.publicKey()) })
+    .then(sourceAccount => {
+      transaction = new StellarSdk.TransactionBuilder(sourceAccount, { fee: 111 })
+        .addOperation(StellarSdk.Operation.payment({
+          destination: dstId,
+          asset: StellarSdk.Asset.native(),
+          amount: process.argv[4]
+        }))
+        .addMemo(StellarSdk.Memo.text('Test Transaction'))
+        .setTimeout(180)
+        .build()
+      transaction.sign(pair)
+      return server.submitTransaction(transaction)
+    })
+    .then(result => logAccountInfo(srcId))
+    .catch(error => console.error(error))
 }
 
 function monitorPayments () {
-  logAccountInfo(pair.publicKey())
+  const dstId = pair.publicKey()
+  const payments = server.payments().forAccount(dstId)
+  const lastToken = lastTokenLoad()
+
+  lastToken && payments.cursor(lastToken)
+  payments.stream({
+    onmessage: payment => {
+      lastTokenSave(payment.paging_token)
+      if (payment.to !== dstId) return;
+
+      const asset = payment.asset_type === 'native' ? 'lumens' :
+        payment.asset_code + ':' + payment.asset_issuer
+
+      console.log(payment.amount, asset, 'from', payment.from)
+    },
+    onerror: error => console.error(error)
+  })
+  process.on('SIGINT', () => logAccountInfo(dstId, () => process.exit(0)))
 }
 
-function logAccountInfo (accountId) {
+function lastTokenLoad () {
+
+}
+
+function lastTokenSave (token) {
+  console.log('token', token)
+}
+
+function logAccountInfo (accountId, f = null) {
   server.accounts().accountId(accountId).call()
-  .then(accountInfo => console.log(accountInfo))
-  .catch(error => console.error(error))
+    .then(accountInfo => {
+      console.log(accountInfo)
+      f && f()
+    })
+    .catch(error => console.error(error))
 }
